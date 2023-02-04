a4799029e51b6ccfac981653c038be7f9adf4b27
Xachik00 <araqelyanxachik48@gmail.com>
2023-02-04 16:00:19 +0400

    end project

--- src/store/action/parlamentActions.ts
+++ src/store/action/parlamentActions.ts
@@ -1,13 +1,13 @@
 import { Dispatch } from "@reduxjs/toolkit";
-import axios from "../../axios";
+// import axios from "../../axios";
 import {  fetching, fetchSuccess, fetchError } from "../slice/parlamentSlice";
 
 export const fetchParlament = ()=>{
     return async (dispatch:Dispatch)=>{
         try{
             dispatch(fetching());
-            const response =await axios.get('parlament');
-            dispatch(fetchSuccess(response.data));
+            // const response =await axios.get('parlament');
+            // dispatch(fetchSuccess(response.data));
         }
         catch(error){
             dispatch(fetchError(error as Error));
