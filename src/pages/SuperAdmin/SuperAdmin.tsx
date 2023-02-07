ea4f2b1cfb1883122611785b7b272c13b285fcbf
Xachik00 <araqelyanxachik48@gmail.com>
2023-02-07 14:41:19 +0400

    end project

--- src/pages/SuperAdmin/SuperAdmin.tsx
+++ src/pages/SuperAdmin/SuperAdmin.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { Header } from '../../components/Header/Header'
 import Register from '../../components/Admin/components/Register';
+import './SuperAdmin.scss'
 
 const SuperAdmin = () => {
 
