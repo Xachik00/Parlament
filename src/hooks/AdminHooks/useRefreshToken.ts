035827eb573176017f68277193beb15eae115c08
Xachik00 <araqelyanxachik48@gmail.com>
2023-02-09 13:25:51 +0400

    final

--- src/hooks/AdminHooks/useRefreshToken.ts
+++ src/hooks/AdminHooks/useRefreshToken.ts
@@ -2,22 +2,24 @@ import axios from '../../axios/axios';
 import useAuth from './useAuth';
 
 const useRefreshToken = () => {
-    const { setAuth }:any = useAuth();
-    const {auth}:any=useAuth()
-    const refreshToken=auth.refreshToken;
-    
+    const { setAuth }: any = useAuth();
+    const { auth }: any = useAuth()
+    const refreshToken = auth.refreshToken;
+
     const refresh = async () => {
-        const response = await axios.post('/refresh', {
-            refreshToken
-        });
-        setAuth((prev:any) => {
-            return {
-                ...prev,
-                role: response.data.role,
-                accessToken: response.data.accessToken
-            }
-        });
-        return response.data.accessToken;
+        if (!auth.accessToken) {
+            const response = await axios.post('/refresh', {
+                refreshToken
+            });
+            setAuth((prev: any) => {
+                return {
+                    ...prev,
+                    role: response.data.role,
+                    accessToken: response.data.accessToken
+                }
+            });
+            return response.data.accessToken;
+        }
     }
     return refresh;
 };
