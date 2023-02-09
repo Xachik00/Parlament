e9a467ef81a3fe2bfb65d5f3cedec99e23d0843d
Xachik00 <araqelyanxachik48@gmail.com>
2023-02-09 17:03:21 +0400

    final

--- src/hooks/AdminHooks/useRefreshToken.ts
+++ src/hooks/AdminHooks/useRefreshToken.ts
@@ -1,27 +1,30 @@
 import axios from '../../axios/axios';
-// import useAuth from './useAuth';
+import useAuth from './useAuth';
 
 const useRefreshToken = () => {
-    // const { setAuth }: any = useAuth();
-    // const { auth }: any = useAuth()
-    // const refreshToken = auth.refreshToken;
+    const { setAuth }:any = useAuth();
+    const {auth}:any=useAuth()
+    const refreshToken=auth.refreshToken;
+    
+    const refresh = async () => {
+        const response = await axios.post('/refresh',
+        JSON.stringify({ refreshToken}),
+        {
+            headers: { 'Content-Type': 'application/json' },
+            withCredentials: true
+        }
+    );
 
-    // const refresh = async () => {
-        // if (!auth?.accessToken) {
-            // const response = await axios.post('/refresh', {
-            //     refreshToken
-            // });
-            // setAuth((prev: any) => {
-            //     return {
-            //         ...prev,
-            //         role: response.data.role,
-            //         accessToken: response.data.accessToken
-            //     }
-            // });
-            // return response.data.accessToken;
-        // }
-    // }
-    // return refresh;
+        setAuth((prev:any) => {
+            return {
+                ...prev,
+                role: response.data.role,
+                accessToken: response.data.accessToken
+            }
+        });
+        return response.data.accessToken;
+    }
+    return refresh;
 };
 
 export default useRefreshToken;
