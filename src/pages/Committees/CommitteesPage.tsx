4b881da53eb64d9917946b03ca755f9f1257b105
Xachik00 <araqelyanxachik48@gmail.com>
2023-02-09 15:13:14 +0400

    final

--- src/pages/Committees/CommitteesPage.tsx
+++ src/pages/Committees/CommitteesPage.tsx
@@ -71,7 +71,7 @@ export const CommitteesPage = () => {
       }
       await axios.post('meets/', newCommites,
       {
-        headers:{'Content-Type': 'application/json',auth_token:auth.accessToken},
+        headers:{'Content-Type': 'application/json',Authorization:auth.accessToken},
         withCredentials: true
       })
       dispatch(fetchCommittees())
