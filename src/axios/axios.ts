3492636172b27f8f0e8e5177964163384f7e9d91
Xachik00 <araqelyanxachik48@gmail.com>
2023-02-09 17:04:23 +0400

    final

--- src/axios/axios.ts
+++ src/axios/axios.ts
@@ -5,3 +5,9 @@ const BASE_URL = 'http://34.125.34.1:3000/api/v1/auth';
 export default axios.create({
     baseURL: BASE_URL
 });
+
+export const axiosPrivate = axios.create({
+    baseURL: BASE_URL,
+    headers: { 'Content-Type': 'application/json' },
+    withCredentials: true
+});
