1516f1351bb873394cb6411cd9ff2f017d799f41
Xachik00 <araqelyanxachik48@gmail.com>
2023-02-09 16:15:31 +0400

    final

--- src/axios/index.ts
+++ src/axios/index.ts
@@ -7,5 +7,5 @@ export default axios.create({
     baseURL: "http://34.125.34.1:3000/api/v1/",
     headers:{
         'Content-Type': 'application/json',
-        'Authorization': `Bearer  ${localStorage.getItem('token')}`}
+        'Authorization': 'Bearer  eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwidXNlciI6InNpbW9ueWFuIiwicm9sZSI6ImFkbWluIiwiY3JlYXRlZF9hdCI6IjIwMjMtMDItMDlUMTA6NDY6MTMuNjc2WiIsInVwZGF0ZWRfYXQiOiIyMDIzLTAyLTA5VDEwOjU3OjQ4Ljg0OFoiLCJpYXQiOjE2NzU5NDQ3MjV9.1LQNMVQdhkSbrSt67uZNb8s9eckm1TyjaNv317B-P-Y'}
 });
