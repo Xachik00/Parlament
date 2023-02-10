34bcfd292813322dd8becf658fa6d88ed17ffeba
Xachik00 <araqelyanxachik48@gmail.com>
2023-02-10 15:43:36 +0400

    final

--- src/axios/axios.ts
+++ src/axios/axios.ts
@@ -2,11 +2,11 @@ import axios from 'axios';
 
 
 export default axios.create({
-    baseURL: process.env.BASE_URL+'3000/api/v1/auth'
+    baseURL: 'http://34.125.34.1:3000/api/v1/auth'
 });
 
 export const axiosPrivate = axios.create({
-    baseURL: process.env.BASE_URL+'auth',
+    baseURL: 'http://34.125.34.1:3000/api/v1/auth',
     headers: { 'Content-Type': 'application/json' },
     withCredentials: true
 });
--- src/axios/index.ts
+++ src/axios/index.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 export default axios.create({
-    baseURL: process.env.BASE_URL+'3000/api/v1/',
+    baseURL: 'http://34.125.34.1:3000/api/v1/',
     headers:{
         'Content-Type': 'application/json',
         'Authorization': `Bearer ${localStorage.getItem('token')}`}
