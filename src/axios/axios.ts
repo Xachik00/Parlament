1d9c25e3c581e2a8df0ff16bf4ebad8fd855e4e7
Xachik00 <araqelyanxachik48@gmail.com>
2023-02-10 16:13:16 +0400

    final

--- src/axios/axios.ts
+++ src/axios/axios.ts
@@ -2,11 +2,11 @@ import axios from 'axios';
 import env from 'react-dotenv'
 
 export default axios.create({
-    baseURL: env.BACK_APP_BASE_URL+'api/v1/auth'
+    baseURL: env.BACK_APP_BASE_URL+'auth'
 });
 
 export const axiosPrivate = axios.create({
-    baseURL: process.env.BASE_URL+'api/v1/auth',
+    baseURL: process.env.BASE_URL+'auth',
     headers: { 'Content-Type': 'application/json' },
     withCredentials: true
 });
--- src/axios/index.ts
+++ src/axios/index.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import env from "react-dotenv";
 
 export default axios.create({
-    baseURL: env.BACK_APP_BASE_URL+'api/v1/',
+    baseURL: env.BACK_APP_BASE_URL,
     headers:{
         'Content-Type': 'application/json',
         'Authorization': `Bearer ${localStorage.getItem('token')}`}
