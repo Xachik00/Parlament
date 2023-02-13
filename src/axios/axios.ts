730d21e7a6e5e41da244caab240358e869bc7f33
Xachik00 <araqelyanxachik48@gmail.com>
2023-02-13 14:38:56 +0400

    final

--- src/axios/axios.ts
+++ src/axios/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
+import env from 'react-dotenv'
 const token=localStorage.getItem('token')
 console.log(token,'token axios');
-import env from 'react-dotenv'
 
 export default axios.create({
     baseURL: 'http://10.10.230.11:3000/api/v1/auth'
