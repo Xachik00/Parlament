877594119f7b9495af88ed5140da8c83a75e4a8c
Xachik00 <araqelyanxachik48@gmail.com>
2023-02-13 11:58:16 +0400

    final

--- src/axios/index.ts
+++ src/axios/index.ts
@@ -1,5 +1,8 @@
 import axios from "axios";
 import env from "react-dotenv";
+const token=localStorage.getItem('token')
+console.log(token);
+console.log(env.BACK_APP_BASE_URL);
 
 export default axios.create({
     baseURL: env.BACK_APP_BASE_URL,
