56d61532fb01afdffd20d72eaba876b859050861
Xachik00 <araqelyanxachik48@gmail.com>
2023-02-13 14:23:06 +0400

    final

--- src/axios/index.ts
+++ src/axios/index.ts
@@ -4,11 +4,9 @@ const token=localStorage.getItem('token')
 console.log(token,'token');
 
 
-export default token?axios.create({
+export default axios.create({
     baseURL: env.BACK_APP_BASE_URL,
     headers:{
         'Content-Type': 'application/json',
         'Authorization': `Bearer ${token}`}
-}):axios.create({
-    baseURL: env.BACK_APP_BASE_URL,
 })
