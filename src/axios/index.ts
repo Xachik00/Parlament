a6c750d41f0781adf690f7bff95a6d044062ede0
Xachik00 <araqelyanxachik48@gmail.com>
2023-02-13 15:57:54 +0400

    final

--- src/axios/index.ts
+++ src/axios/index.ts
@@ -2,13 +2,11 @@ import axios from "axios"
 import env from "react-dotenv";
 import { useEffect,useState } from "react";
 
-const [token,setToken]=useState<any>();
+const [token,setToken]=useState<any>(localStorage.getItem('token'));
 
-useEffect(()=>{
-    
-    setToken(localStorage.getItem('token'))
-    
-},[localStorage]) 
+// useEffect(()=>{
+//     setToken(localStorage.getItem('token')) 
+// },[localStorage]) 
 
 export default axios.create({
     baseURL: env.BACK_APP_BASE_URL,
